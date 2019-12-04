0e5569f5b9a1ea3ae6f032c80e65fe20aebf4c39
akshaypatil9503 <akshay.patil@galentictech.com>
2019-12-04 17:25:48 +0530

    Edit employee data added

--- src/modules/execution/TestExecution.java
+++ src/modules/execution/TestExecution.java
@@ -7,20 +7,20 @@ public class TestExecution {
 	public static void main(String[] args) {
 //		
 //			String testCases [] = {"newemp_001","newemp_002","newemp_003"};
-		
-//		String testCases [] = {"newemp_001"};
+//		
 //			for (int i = 0; i < testCases.length; i++) {
 //				TestDriver.tDriver(testCases[i],"EmployeeConfig","NewEmp");
 //			}
 			
-		String testCases [] = {"editemp_001"};
-//		String testCases [] = {"editemp_001","editemp_002","editemp_003","editemp_004"};
+		String testCasesNew [] = {"editemp_007"};
+//		String testCasesNew [] = {"editemp_001","editemp_002","editemp_003","editemp_004","editemp_005","editemp_006"};
 	
-			for (int i = 0; i < testCases.length; i++) {
-				 TestDriver.tDriver(testCases[i],"EmployeeConfig","EditEmp");
+			for (int i = 0; i < testCasesNew.length; i++) {
+				 TestDriver.tDriver(testCasesNew[i],"EmployeeConfig","EditEmp");
 			}
 			
 			
 	}
 
 }
+
